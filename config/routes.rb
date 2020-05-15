c512d8a2d3d1dcceec5bacabd2adbe6fd866252e
Astrid Boisson <astrid.bdc@gmail.com>
2020-05-15 07:58:44 +0200

    ok cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :restaurants, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:create]
   end
+  resources :reviews, only: [:destroy]
 end
